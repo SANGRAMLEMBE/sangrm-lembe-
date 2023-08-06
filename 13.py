8ec71e22b0219b96f77bbf3bbebc9ed7d2993cf7
Sangram pradip lembe <102941955+SANGRAMLEMBE@users.noreply.github.com>
2023-08-06 22:04:04 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,11 @@
+#for division we use forward slash it gives us float value
+print(1235/2)
+#when we //then it gives us int value
+print(1235//2)
+#for making multiplication we use *
+print(2*3)
+#but when wants to power then we use **
+print(2**3)
+#here for entering value in input we have take loop
+a,b,c=[int(x) for x in input("enter values").split()]
+#split is use for 1] () for spacebar 2]"" in "" we can use any key for enter or basically it replaced at enter
